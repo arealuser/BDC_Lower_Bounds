3489a206b9f3934eb54e2a6b676bb8ee28236cd7
Ittai Rubinstein <ittai.rubinstein@gmail.com>
2022-12-09 23:40:17 +0200

    Refactored to separate into different logical portions of the project.

---
+++ log_arithmetics.py
@@ -0,0 +1,85 @@
+from typing import Union
+
+import numpy as np
+
+from code_for_lower_bounds.rzk_tables import LOG_FACTORIALS
+
+
+def safe_log_arr(arr: np.ndarray):
+    """
+    Computes the logarithm of arr using numpy, but ensuring that 0s are mapped to -inf.
+    This ensures that for any non-negative array arr: np.exp(safe_log(arr)) will be equal to arr.
+    """
+    return np.log(arr, out=-np.inf * np.ones(np.shape(arr)), where=(arr != 0))
+
+
+def safe_log_scalar(scalar: Union[int, float, np.ndarray]):
+    """
+    Computes the logarithm of arr using numpy, but ensuring that 0s are mapped to -inf.
+    This ensures that for any non-negative array arr: np.exp(safe_log(arr)) will be equal to arr.
+    """
+    if scalar == 0:
+        return -np.inf
+    return np.log(scalar)
+
+
+def log_factorial(k):
+    """
+    Compute the logarithm of the factorial of k.
+    """
+    return float(np.sum(np.log(np.arange(1, k + 1))))
+
+
+def log_sum(arr):
+    """
+    Compute log(sum(exp(arr)))
+    """
+    m = np.max(arr)
+    m = np.nan_to_num(m)  # Avoid cases where m is negative infinity.
+    return m + np.log(np.sum(np.exp(arr - m)))
+
+
+def log_add(a, b):
+    """
+    Compute log(exp(a) + exp(b))
+    """
+    m = max(a, b)
+    m = np.nan_to_num(m)  # Avoid cases where m is negative infinity.
+    return m + np.log(np.exp(a - m) + np.exp(b - m))
+
+
+def log_sub(a, b):
+    """
+    Compute log(exp(a) - exp(b))
+    """
+    m = max(a, b)
+    m = np.nan_to_num(m)  # Avoid cases where m is negative infinity.
+    return m + safe_log_scalar(np.exp(a - m) - np.exp(b - m))
+
+
+def log_sub_arr(a, b):
+    """
+    Compute log(exp(a) - exp(b))
+    """
+    m = np.maximum(a, b)
+    m = np.nan_to_num(m)  # Avoid cases where m is negative infinity.
+    return m + safe_log_arr(np.exp(a - m) - np.exp(b - m))
+
+
+def log_over(n, k):
+    """
+    Returns the logarithm of n choose k.
+    """
+    if k > n:
+        return -np.inf
+    return LOG_FACTORIALS[n] - (LOG_FACTORIALS[k] + LOG_FACTORIALS[n - k])
+
+
+def log_over_arr(n, k):
+    """
+    Returns the logarithm of n choose k when they are given as arrays.
+    When n is smaller than k, the result is clipped to return 0.
+    """
+    res = LOG_FACTORIALS[n] - (LOG_FACTORIALS[k] + LOG_FACTORIALS[np.clip(n - k, 0, None)])
+    res[n < k] = -np.inf
+    return res
---
+++ rzk_tables.py
@@ -0,0 +1,103 @@
+import time
+
+import numpy as np
+import tqdm
+
+from code_for_lower_bounds.log_arithmetics import log_sub, log_over, log_sub_arr, log_over_arr, log_factorial
+
+LOG_FACTORIALS: np.ndarray = np.zeros(1)
+RZK_TABLE: np.ndarray = np.zeros(1)
+
+
+def get_rzk_slow_PRC(r, z, k):
+    """
+    Compute log((((r+z) ^ k) - (r ^ k)) / (k!))
+    """
+    global LOG_FACTORIALS
+    if r == 0:
+        return (k * np.log(r + z)) - LOG_FACTORIALS[k]
+    return log_sub(k * np.log(r + z), k * np.log(r)) - LOG_FACTORIALS[k]
+
+
+def get_rzk_slow_BDC(r, z, k):
+    """
+    Compute log(over(r+z,k) - over(r,k))
+    """
+    global LOG_FACTORIALS
+    return log_sub(log_over(r + z, k), log_over(r, k))
+
+
+def get_rzk_less_slow_PRC(rs, zs, ks):
+    """
+    Compute log((((r+z) ^ k) - (r ^ k)) / (k!)), using numpy vectorization.
+    """
+    return log_sub_arr(ks * np.log(rs + zs), ks * np.log(rs)) - LOG_FACTORIALS[1:]
+
+
+def get_rzk_less_slow_BDC(rs, zs, ks):
+    """
+    Compute log(over(r+z, k) - over(r,k)), using numpy vectorization.
+    """
+    return log_sub_arr(log_over_arr(rs + zs, ks), log_over_arr(rs, ks))
+
+
+def get_rzk_cache(r, z, k):
+    """
+    Returns the rzk value from the cache.
+    If we are dealing with a PRC, then this should be equal to log((((r+z) ^ k) - (r ^ k)) / (k!)).
+    If we are dealing with a BDC, then this should be equal to log(over(r+z, k) - over(r,k)) (with log(0)=-np.inf).
+    """
+    global RZK_TABLE
+    return RZK_TABLE[r, z, k]
+
+
+def get_log_transition_prob_rzkl(r, z, k, lam):
+    """
+    Return log(lam^k e^(-lam) ((r+z)^k - r^k) / k!)
+    """
+    return (k * np.log(lam)) + get_rzk_cache(r, z, k) - lam * (r + z)
+
+
+def compute_RZK_table_PRC(r_max, z_max, k_max):
+    """
+    Computes the logarithm of the combinatorial formula used for computing lower bounds on the capacity of the PRC.
+    """
+    t0 = time.time()
+    global LOG_FACTORIALS, RZK_TABLE
+    LOG_FACTORIALS = np.array([0.0] + [log_factorial(k) for k in range(1, k_max)])
+
+    print(f'Generating cache table of size {r_max}x{z_max}x{k_max} (~%.1f bits)...' % (np.log2(r_max * z_max * k_max)))
+
+    RZK_TABLE = np.zeros((r_max, z_max, k_max))
+    rs = np.reshape(np.arange(1, r_max), (-1, 1, 1))
+    zs = np.reshape(np.arange(1, z_max), (1, -1, 1))
+    ks = np.reshape(np.arange(1, k_max), (1, 1, -1))
+    RZK_TABLE[1:, 1:, 1:] = get_rzk_less_slow_PRC(rs, zs, ks)
+    for z in tqdm.trange(1, z_max):
+        for k in range(1, k_max):
+            RZK_TABLE[0, z, k] = get_rzk_slow_PRC(0, z, k)
+
+    print('Done. Table generation took %.1f seconds' % (time.time() - t0))
+
+
+def compute_RZK_table_BDC(r_max, z_max, k_max):
+    """
+    Computes the logarithm of the combinatorial formula used for computing lower bounds on the capacity of the BDC.
+    """
+    t0 = time.time()
+    global LOG_FACTORIALS, RZK_TABLE
+    LOG_FACTORIALS = np.array([0.0] + [log_factorial(k) for k in range(1, k_max + r_max + z_max + 1)])
+
+    print(f'Generating cache table of size {r_max}x{z_max}x{k_max} (~%.1f bits)...' % (np.log2(r_max * z_max * k_max)))
+
+    RZK_TABLE = np.zeros((r_max, z_max, k_max))
+    rs = np.reshape(np.arange(1, r_max), (-1, 1, 1))
+    zs = np.reshape(np.arange(1, z_max), (1, -1, 1))
+    ks = np.reshape(np.arange(1, k_max), (1, 1, -1))
+    RZK_TABLE[1:, 1:, 1:] = get_rzk_less_slow_BDC(rs, zs, ks)
+    for z in tqdm.trange(1, z_max):
+        for k in range(1, k_max):
+            RZK_TABLE[0, z, k] = get_rzk_slow_BDC(0, z, k)
+
+    print('Done. Table generation took %.1f seconds' % (time.time() - t0))
+    
